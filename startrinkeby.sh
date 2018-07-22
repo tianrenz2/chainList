d15d57c104a4b6dfc1941ac080eefc49ad0717dc
tianrenz2 <tianrenz@uci.edu>
2018-07-21 20:02:47 -0700

    adding frontend files to pages on Github

---
+++ startrinkeby.sh
@@ -0,0 +1,1 @@
+geth --rinkeby --rpc --rpcapi="personal,eth,network,wbe3,net" --ipcpath "~/Library/Ethereum/geth.ipc"
--- truffle.js
+++ truffle.js
@@ -12,6 +12,14 @@ module.exports = {
             port: 8545,
             network_id: "4224",
             gas: 4700000
+          },
+          rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: 4,
+            gas: 4700000
           }
      }
+
+
 };
